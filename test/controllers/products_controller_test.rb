b94fb4c359de030a357d4c896ce49ec296f7b0d2
eddxw <eduardo.jemb@gmail.com>
2018-09-17 11:59:30 -0700

    Iteration E3

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -59,9 +59,9 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "can't delete product in cart" do
     assert_difference('Product.count', 0) do
-      delete product_url(product(:two))
+      delete product_url(products(:two))
     end
-    assert_redirected_to products_urlsss
+    assert_redirected_to products_url
   end
 
   test "should destroy product" do
