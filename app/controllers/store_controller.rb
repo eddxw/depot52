a6d78a5183b3b8ff1a29f4f56f74ad56cb1b8e1f
eddxw <eduardo.jemb@gmail.com>
2018-09-17 17:02:01 -0700

    Task F1: Moving the Cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,6 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
   def index
     @products = Product.order(:title)
     session[:counter].nil? ? session[:counter] ||= 1 : session[:counter] += 1
